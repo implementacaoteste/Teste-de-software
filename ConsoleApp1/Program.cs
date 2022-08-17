743140726d2eb4a94d0e902dc8766c35e6f60d6b
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-08-17 16:56:47 -0300

    Conclusão do primeiro exemplo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(AplicarDesconto(10));
+        }
+
+        public static double AplicarDesconto(double _valor)
+        {
+            double retorno;
+
+            if (_valor < 1000 && _valor > 500)
+                retorno = _valor - _valor * 0.1;
+            else if (_valor > 1000)
+                retorno = _valor - _valor * 0.15;
+            else
+                retorno = _valor;
+
+            return retorno;
+        }
+    }
+}
